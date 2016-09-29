6f4c445b24506a0e6646a8a8777a6dda6e6cac74
Kenzo Mendoza <kenzom954@gmail.com>
2016-09-28 21:45:57 -0700

    created axis for x and y

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,13 +1,84 @@
 $( document ).ready(function(){
   const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  const w = 800;
+  const h = 450;
+  function render(data){
+    const margin = {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50
+    }
+    const width = w - (margin.left + margin.right);
+    const height = h - (margin.top + margin.bottom);
+    //create canvas with id="chart"
+    const svg = d3.select("body")
+                  .append("svg")
+                    .attr("id","chart")
+                    .attr("width", w)
+                    .attr("height", h)
 
+    const chart = svg.append("g")
+                    .classed("display", true)
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const dateParser = d3.timeParse("%Y-%m-%d");
+
+    const x = d3.scaleTime()
+                  .domain(d3.extent(data, function(d){
+                    let date = dateParser(d[0])
+                    return date
+                  }))
+                  .range([0,width]);
+
+    const y = d3.scaleLinear()
+                  .domain([0, d3.max(data, function(d){
+                    return d[1]
+                  })])
+                  .range([height, 0]);
+
+    const xAxis = d3.axisBottom(x)
+                      .tickFormat(d3.timeFormat("%Y"));
+
+    const yAxis = d3.axisLeft(y);
+
+    function plot(params){
+      //create axis for x and y
+      this.append("g")
+        .classed("x axis", true)
+        .attr("transform","translate(0,"+ height +")")
+        .call(params.axis.x)
+
+      this.append("g")
+        .classed("y axis", true)
+        .attr("transform", "translate(0,0)")
+        .call(params.axis.y)
+      //enter()
+
+      //update
+
+      //exit
+    }
+
+    plot.call(chart,{
+      data: data,
+      axis:{
+        x: xAxis,
+        y: yAxis
+      }
+    })
+
+  }
   $.ajax({
     type: "GET",
     dataType: "json",
     url: url,
-    beforeSend: ()=> console.log('beforeSend'),
+    beforeSend: ()=> {
+      console.log('beforeSend')
+    },
     success: (data) =>{
-      console.log(data)
+      //sent data plots to render function
+      render(data.data)
     },
     fail: () =>{
       console.log('failure!')
@@ -15,5 +86,5 @@ $( document ).ready(function(){
     error: () =>{
       console.log('error!')
     }
-});
+  });
 });
